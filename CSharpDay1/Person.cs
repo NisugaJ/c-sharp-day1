a25dc3f4fc6877fb5509d83f69ab98b3b4f65f8c
Nisuga Jayawardana <nisuga@rockwell@gmail.com>
2023-09-05 22:03:03 +0100

    test commit

--- CSharpDay1/Person.cs
+++ CSharpDay1/Person.cs
@@ -3,6 +3,8 @@ namespace CSharpDay1
 {
     internal class Person
     {
+        // test comment 
+
         private string lastName;
         public string? FirstName { get; set; }
         public required string LastName // 'required' attributed can assigned a value only at initialization
